8277a2cda1542d6dffe532d12b0f5b78ae32f985
livevie <ela661991@gmail.com>
2020-03-30 15:59:40 +0200

    added size component content

--- src/app/design-tokens/size/size.component.ts
+++ src/app/design-tokens/size/size.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SizeComponent implements OnInit {
 
+  public sizes = [
+    {
+      img: '/assets/graphics/design-tokens/sizes/size1.svg',
+      alt: 'Size 1 Graphic',
+      desc: 'Time'
+    },
+    {
+      img: '/assets/graphics/design-tokens/sizes/size2.svg',
+      alt: 'Size 2 Graphic',
+      desc: 'Notification'
+    },
+    {
+      img: '/assets/graphics/design-tokens/sizes/size3.svg',
+      alt: 'Size 3 Graphic',
+      desc: 'Completion'
+    },
+    {
+      img: '/assets/graphics/design-tokens/sizes/size4.svg',
+      alt: 'Size 4 Graphic',
+      desc: 'Error'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
