06612ddb975b7d1f638a507ba5865a5c6bc1fcea
livevie <ela661991@gmail.com>
2020-03-30 11:53:30 +0200

    added line content and graphics

--- src/app/design-tokens/line/line.component.ts
+++ src/app/design-tokens/line/line.component.ts
@@ -7,6 +7,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LineComponent implements OnInit {
 
+  public lines = [
+    {
+      img: '/assets/graphics/lines/graphic_line1.svg',
+      alt: 'Line 1 Graphic',
+      style: 'Solid',
+      weight: '0.06em',
+      start: 'none',
+      end: 'Filled Arrow'
+    },
+    {
+      img: '/assets/graphics/lines/graphic_line2.svg',
+      alt: 'Line 2 Graphic',
+      style: 'Dashed',
+      weight: '0.06em',
+      start: 'Filled Arrow',
+      end: 'none'
+    },
+    {
+      img: '/assets/graphics/lines/graphic_line3.svg',
+      alt: 'Line 3 Graphic',
+      style: 'Dashed',
+      weight: '0.06em',
+      start: 'none',
+      end: 'Filled Arrow'
+    },
+    {
+      img: '/assets/graphics/lines/graphic_line4.svg',
+      alt: 'Line 4 Graphic',
+      style: 'Dashed',
+      weight: '0.06em',
+      start: 'none',
+      end: 'Filled Arrow'
+    },
+    {
+      img: '/assets/graphics/lines/graphic_line5.svg',
+      alt: 'Line 5 Graphic',
+      style: 'Dashed',
+      weight: '0.06em',
+      start: 'none',
+      end: 'none'
+    },
+    {
+      img: '/assets/graphics/lines/graphic_line6.svg',
+      alt: 'Line 6 Graphic',
+      style: 'Solid',
+      weight: '0.06em',
+      start: 'none',
+      end: 'none'
+    },
+    {
+      img: '/assets/graphics/lines/graphic_line7.svg',
+      alt: 'Line 7 Graphic',
+      style: 'Solid',
+      weight: '0.06em',
+      start: 'none',
+      end: 'none'
+    },
+    {
+      img: '/assets/graphics/lines/graphic_line8.svg',
+      alt: 'Line 8 Graphic',
+      style: 'Dashed',
+      weight: '0.06em',
+      start: 'none',
+      end: 'Filled Arrow'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
