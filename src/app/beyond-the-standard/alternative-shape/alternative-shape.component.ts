2f9b891b8ed86fe893950f5fd620d3a95c85d6aa
livevie <ela661991@gmail.com>
2020-04-06 12:01:49 +0200

    added graphics open-issues and beyond the standard, added small changes

--- src/app/beyond-the-standard/alternative-shape/alternative-shape.component.ts
+++ src/app/beyond-the-standard/alternative-shape/alternative-shape.component.ts
@@ -11,7 +11,41 @@ export class AlternativeShapeComponent implements OnInit {
     {
       img: '/assets/graphics/beyond-the-standard/alternative-shape/alternative-shape1.svg',
       alt: 'Alternative Shape 1 Graphic',
-      styleClass: 'bigger'
+      desc: 'Cloud'
+    },
+    {
+      img: '/assets/graphics/beyond-the-standard/alternative-shape/alternative-shape2.svg',
+      alt: 'Alternative Shape 1 Graphic',
+      desc: 'Bottle'
+    },
+    {
+      img: '/assets/graphics/beyond-the-standard/alternative-shape/alternative-shape3.svg',
+      alt: 'Alternative Shape 4 Graphic',
+      desc: 'Emoji'
+    },
+    {
+      img: '/assets/graphics/beyond-the-standard/alternative-shape/alternative-shape4.svg',
+      alt: 'Alternative Shape 4 Graphic',
+      desc: 'Rounded Rectangle'
+    }
+  ];
+
+  public ownAlternativeShapes = [
+    {
+      img: '/assets/graphics/beyond-the-standard/alternative-shape/alternative-shape5.svg',
+      alt: 'Alternative Shape 5 Graphic'
+    },
+    {
+      img: '/assets/graphics/beyond-the-standard/alternative-shape/alternative-shape6.svg',
+      alt: 'Alternative Shape 6 Graphic'
+    },
+    {
+      img: '/assets/graphics/beyond-the-standard/alternative-shape/alternative-shape7.svg',
+      alt: 'Alternative Shape 7 Graphic'
+    },
+    {
+      img: '/assets/graphics/beyond-the-standard/alternative-shape/alternative-shape8.svg',
+      alt: 'Alternative Shape 8 Graphic'
     }
   ];
 
