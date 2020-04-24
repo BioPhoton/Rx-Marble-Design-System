4225b5028d26947577e3dc5c165fb4d9843d61a7
livevie <ela661991@gmail.com>
2020-04-24 16:34:29 +0200

    added layout for diagram-details

--- src/app/mds-diagrams/diagrams.ts
+++ src/app/mds-diagrams/diagrams.ts
@@ -11,6 +11,7 @@ export const mdsDiagrams: MdsDiagram[] = [
     hidden: false, class: 'rate-limit',
     desc: 'audit',
     image: 'https://github.com/BioPhoton/Rx-Marble-Design-System/blob/dev/assets/operators/new/rate-limiting/audit.png?raw=true',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
   },
   {
     hidden: false, class: 'rate-limit',
@@ -23,7 +24,8 @@ export const mdsDiagrams: MdsDiagram[] = [
   },
   {
     hidden: false, class: 'buffer',
-    desc: 'bufferCount'
+    desc: 'bufferCount',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
   },
   {
     hidden: false, class: 'buffer',
