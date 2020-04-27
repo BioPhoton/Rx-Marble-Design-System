2a065910f52f3b2d4ed5acbffb2f15f243f97a33
livevie <ela661991@gmail.com>
2020-04-27 12:38:20 +0200

    added icons to childnodes and added changed list-item-active styles

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -30,26 +30,32 @@ const TREE_DATA: NavNode[] = [
           {
             routerLink: '/design-tokens/unit',
             name: 'Unit',
+            icon: 'remove'
           },
           {
             routerLink: '/design-tokens/font',
             name: 'Font',
+            icon: 'remove'
           },
           {
             routerLink: '/design-tokens/color',
             name: 'Color',
+            icon: 'remove'
           },
           {
             routerLink: '/design-tokens/line',
             name: 'Line',
+            icon: 'remove'
           },
           {
             routerLink: '/design-tokens/shape',
             name: 'Shape',
+            icon: 'remove'
           },
           {
             routerLink: '/design-tokens/size',
             name: 'Size',
+            icon: 'remove'
           }
         ]
       },
@@ -60,46 +66,57 @@ const TREE_DATA: NavNode[] = [
           {
             routerLink: '/components/overview',
             name: 'Overview',
+            icon: 'remove'
           },
           {
             routerLink: '/components/observable',
             name: 'Observable',
+            icon: 'remove'
           },
           {
             routerLink: '/components/time-span',
             name: 'Time Span',
+            icon: 'remove'
           },
           {
             routerLink: '/components/event-in-time',
             name: 'Event in Time',
+            icon: 'remove'
           },
           {
             routerLink: '/components/operation',
             name: 'Operation',
+            icon: 'remove'
           },
           {
             routerLink: '/components/consumer-event',
             name: 'Consumer Event',
+            icon: 'remove'
           },
           {
             routerLink: '/components/notifications',
             name: 'Notifications',
+            icon: 'remove'
           },
           {
             routerLink: '/components/completion',
             name: 'Completion',
+            icon: 'remove'
           },
           {
             routerLink: '/components/error',
             name: 'Error',
+            icon: 'remove'
           },
           {
             routerLink: '/components/operator',
             name: 'Operator',
+            icon: 'remove'
           },
           {
             routerLink: '/components/task-stacks',
             name: 'Task Stacks',
+            icon: 'remove'
           }
         ]
       },
@@ -110,82 +127,102 @@ const TREE_DATA: NavNode[] = [
           {
             routerLink: '/diagrams/sections',
             name: 'Sections',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/description',
             name: 'Description',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/legend',
             name: 'Legend',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/flow-description',
             name: 'Flow Description',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/diagram-canvas',
             name: 'Diagram Canvas',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/grid',
             name: 'Grid',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/positioning',
             name: 'Positioning',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/notification-and-content',
             name: 'Notification and Content',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/notification-and-shape',
             name: 'Notification and Shape',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/notification-and-color',
             name: 'Notification and Color',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/multiple-notifications',
             name: 'Multiple Notifications',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/notification-and-dynamic-width',
             name: 'Notification and Dynamic Width',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/notification-and-inner-components',
             name: 'Notification and Inner Components',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/inner-state',
             name: 'Inner State',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/operator-padding',
             name: 'Operator Padding',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/operator-and-line',
             name: 'Operator and Line',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/multiple-operations',
             name: 'Multiple Operations',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/operator-nesting',
             name: 'Operator Nesting',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/inactive-components',
             name: 'Inactive Components',
+            icon: 'remove'
           },
           {
             routerLink: '/diagrams/examples-per-operator',
             name: 'Examples per Operator',
+            icon: 'remove'
           }
         ]
       },
@@ -196,18 +233,22 @@ const TREE_DATA: NavNode[] = [
           {
             routerLink: '/beyond-the-standard/alternative-shape',
             name: 'Alternative Shape',
+            icon: 'remove'
           },
           {
             routerLink: '/beyond-the-standard/alternative-units',
             name: 'Alternative Units',
+            icon: 'remove'
           },
           {
             routerLink: '/beyond-the-standard/todos',
             name: 'ToDos',
+            icon: 'remove'
           },
           {
             routerLink: '/beyond-the-standard/vertical-layout',
             name: 'Vertical Layout',
+            icon: 'remove'
           }
         ]
       },
@@ -218,6 +259,7 @@ const TREE_DATA: NavNode[] = [
           {
             routerLink: '/open-issues/operations-and-their-end',
             name: 'Operation and their end',
+            icon: 'remove'
           }
         ]
       },
