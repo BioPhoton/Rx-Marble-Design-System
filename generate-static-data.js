ac648a071bf2723507a43c3d9ce01e8387ed7cfa
Vojtech Mašek <vojtech.masek@flowup.cz>
2020-12-17 19:57:38 +0100

    build(): custom webpack builder refactored to pre build npm script

---
+++ generate-static-data.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+
+const folderName = './src/static/diagrams';
+const staticDataFiles = fs
+  .readdirSync(folderName)
+  .filter(file => file.endsWith('.json'))
+  .sort()
+  .map(file =>
+    JSON.parse(fs.readFileSync(path.join(folderName, file), 'utf-8')),
+  );
+
+function mergeSinglesToDictionary() {
+  return JSON.stringify(
+    Object.assign({}, ...staticDataFiles.map(s => ({ [s.name]: s }))),
+    null,
+    2,
+  );
+}
+
+fs.writeFileSync('./src/static/diagrams.json', mergeSinglesToDictionary());
